5c2a23327dc215bc560994d76eb2bc107ed281d8
yohang <yohan@giarelli.org>
2012-06-05 17:19:04 +0200

    Added UpcomingEventsBlock

---
+++ EventListener/BlockListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Soloist\Bundle\CalendarBundle\EventListener;
+
+use Doctrine\ORM\EntityManager;
+
+use Soloist\Bundle\CalendarBundle\Form\Type\BlockSettings\UpcomingEventsType,
+    Soloist\Bundle\BlockBundle\EventListener\Event\RequestTypes;
+
+class BlockListener
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function onRequestTypes(RequestTypes $event)
+    {
+        $event->getManager()->addBlockType('upcoming_events', array(
+            'name'          => 'Prochains événements',
+            'action'        => 'SoloistCalendarBundle:Default:showUpcomings',
+            'settings'      => array('calendar' => null, 'nb' => 5),
+            'form'          => new UpcomingEventsType($this->em),
+        ));
+    }
+}
